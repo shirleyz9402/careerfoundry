511001b1afcee129f40e2e61a283d70997902c26
shirleyz9402 <shirleyz9402@yahoo.com>
2018-08-26 17:44:14 -0400

    buttons on buttons on buttons

--- frontend-challenge/src/App.js
+++ frontend-challenge/src/App.js
@@ -2,17 +2,56 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const IPLink = 'http://api.ipstack.com/check?access_key=df3b805c8ac6ed141ca9e04658cb8917'
+
 class App extends Component {
+  state = {
+    courses: [],
+    selectedCourse: null,
+    country: '',
+    continent:''
+  }
+  componentDidMount(){
+    fetch('https://careerfoundry.com/en/api/courses').then(r => r.json()).then(r => this.setState({courses: Object.keys(r.courses).map(course => r.courses[course])})).then(this.fetchLocation())
+  }
+  fetchLocation = () => {
+    fetch(IPLink)
+    .then(r => r.json())
+    .then(r => this.setState({country: r.country_code, continent: r.continent_code}))
+  }
+  setLocation = () => {
+    if(this.state.country === "GB"){
+      return "UK"
+    }
+    else if (this.state.continent === "EU" && this.state.country !== "GB"){
+      return "EU"
+    }
+    else {
+      return "NA"
+    }
+  }
+  handleClick = (course) => {
+    console.log(course)
+    this.setState({selectedCourse: course})
+  }
+
   render() {
+    console.log(this.state)
+    const renderCourses = this.state.courses.map(course =>{
+      return(
+        <div className="course" id={course.title} value={course} onClick={(event) => this.handleClick(course)}>
+          <button>
+            <p>{course.title}</p>
+            <p>View Course Details</p>
+          </button>
+        </div>
+      )
+    })
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <div className="App">
+          {this.state.selectedCourse ? <Course selectedCourse= {this.state.selectedCourse} handleBack={this.handleBack} location={this.setLocation()}/> : <div className="courses"> {renderCourses} </div>}
+        </div>
       </div>
     );
   }
